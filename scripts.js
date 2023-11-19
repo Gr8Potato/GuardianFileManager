05182111b34bb8e0cba51f29b51a978bc130efcb
Aidan Eiler <daultimatepotato@gmail.com>
2023-11-18 22:21:25 -0600

    added backend logic to project

--- scripts.js
+++
@@ -1,9 +0,0 @@
-//to make sure that the passwords match in account creation
-function check(input) {
-    if (input.value != document.getElementById('password').value) {
-      input.setCustomValidity('Password must be matching.');
-    } else {
-      // input is valid -- reset the error message
-      input.setCustomValidity('');
-    }
-  }
