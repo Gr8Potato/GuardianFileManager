af27e09c7c02934614ad4c01b375341eddac92cb
Aidan Eiler <daultimatepotato@gmail.com>
2023-11-25 01:57:16 -0600

    part 1 of slay branch changes

--- main.php
+++ main.php
@@ -23,8 +23,7 @@
             <div class="titleHeader">
 
                 <form id="upload_form" action="uploadhandler" method="post" enctype="multipart/form-data">
-                    Select file to upload:
-                    <input type="file" name="filesToUpload[]" id="fileToUpload" multiple>
+                    <input class="btn" type="file" name="filesToUpload[]" id="fileToUpload" multiple>
                     <input id="upload_button" type="submit" class="btn" value="Upload File" name="submit">
                 </form>
                 <div id="status_container"></div>
@@ -39,52 +38,54 @@
         </div>
     </header>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Creation Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $user_dir = "/home/" . $_SESSION["user"];
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Creation Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $user_dir = "/home/" . $_SESSION["user"];
 
-            if (!is_dir($user_dir)) {
-                echo "<tr><td colspan='3'>Directory not found</td></tr>";
-            }
-
-            $files = [];
-            $exclude = array('.bash_history', '.cache', '.bash_logout', '.config', '.local', '.bashrc', '.profile', 'snap'); //we dont want the user to see or change these directories
-            
-            foreach (new DirectoryIterator($user_dir) as $fileInfo) {
-                if ($fileInfo->isDot() || in_array($fileInfo->getFilename(), $exclude)) { //excludes . and .. as well as the excluded files & folders
-                    continue;
+                if (!is_dir($user_dir)) {
+                    echo "<tr><td colspan='3'>Directory not found</td></tr>";
                 }
 
-                $files[] = [
-                    'name' => $fileInfo->getFilename(),
-                    'date' => date("m/d/Y", $fileInfo->getMTime()),
-                    'id' => $fileInfo->getInode()
-                ];
-            }
+                $files = [];
+                $exclude = array('.bash_history', '.cache', '.bash_logout', '.config', '.local', '.bashrc', '.profile', 'snap'); //we dont want the user to see or change these directories
+                
+                foreach (new DirectoryIterator($user_dir) as $fileInfo) {
+                    if ($fileInfo->isDot() || in_array($fileInfo->getFilename(), $exclude)) { //excludes . and .. as well as the excluded files & folders
+                        continue;
+                    }
 
-            //creates a row for each file, with an ID for each row. the id design choice should not posses a security risk to the system
-            foreach ($files as $file) {
-                echo "<tr id='file" . htmlspecialchars($file['id']) . "'>";
-                echo "<td>" . htmlspecialchars($file['name']) . "</td>";
-                echo "<td>" . $file['date'] . "</td>";
-                echo "<td>";
-                echo "<button class='preview-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/eye.png' alt='Preview'></button>";
-                echo "<button class='download-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/download.png' alt='Download'></button>";
-                echo "<button class='delete-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/trash.png' alt='Delete'></button>";
-                echo "</td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+                    $files[] = [
+                        'name' => $fileInfo->getFilename(),
+                        'date' => date("m/d/Y", $fileInfo->getMTime()),
+                        'id' => $fileInfo->getInode()
+                    ];
+                }
+
+                //creates a row for each file, with an ID for each row. the id design choice should not posses a security risk to the system
+                foreach ($files as $file) {
+                    echo "<tr id='file" . htmlspecialchars($file['id']) . "'>";
+                    echo "<td>" . htmlspecialchars($file['name']) . "</td>";
+                    echo "<td>" . $file['date'] . "</td>";
+                    echo "<td>";
+                    echo "<button class='preview-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/eye.png' alt='Preview'></button>";
+                    echo "<button class='download-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/download.png' alt='Download'></button>";
+                    echo "<button class='delete-button' data-filename='" . htmlspecialchars($file['name']) . "'><img src='Icons/trash.png' alt='Delete'></button>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
